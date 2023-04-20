bd830c4954887180274cf9ead281c95eca2d05ee
andyrose507 <drew.jones1998@gmail.com>
2023-04-19 20:32:32 -0700

    add html body structure

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+    # echo 'Hello World'
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Regular Expression Tester</title>
+</head>
+<body>
+    <div id="rt-container">
+        <div id="rt-title">Regular Expression Tester</div>
+        <div id="rt-content">
+            <div id="rt-inputs"></div>
+            <div id="rt-attempts"></div>
+        </div>
+    </div>
+</body>
+</html>
