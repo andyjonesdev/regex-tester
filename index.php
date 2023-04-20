bffdf2d394a679fe239301ae95b0cb5a7a5ace85
andyrose507 <drew.jones1998@gmail.com>
2023-04-20 00:00:12 -0700

    add html form elements

--- index.php
+++ index.php
@@ -18,7 +18,26 @@
     <div id="rt-container">
         <div id="rt-title">Regular Expression Tester</div>
         <div id="rt-content">
-            <div id="rt-inputs"></div>
+            <div id="rt-inputs">
+                <form>
+                    <label for="regex">Regular Expression</label><br>
+                    <input type="text" name="regex"><br>
+
+                    <label for="test-lines">Expressions to Test</label><br>
+                    <input type="text" name="text-lines" id="line-1"><br>
+                    <input type="text" name="text-lines" id="line-2"><br>
+                    <input type="text" name="text-lines" id="line-3"><br>
+                    <input type="text" name="text-lines" id="line-4"><br>
+                    <input type="text" name="text-lines" id="line-5"><br>
+                    <input type="text" name="text-lines" id="line-6"><br>
+                    <input type="text" name="text-lines" id="line-7"><br>
+                    <input type="text" name="text-lines" id="line-8"><br>
+                    <input type="text" name="text-lines" id="line-9"><br>
+                    <input type="text" name="text-lines" id="line-10"><br>
+
+                    <input type="button" value="Test Lines">
+                </form>
+            </div>
             <div id="rt-attempts"></div>
         </div>
     </div>
