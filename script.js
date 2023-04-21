30c4457f51679463fc041fb2e5123016c0705bfc
andyrose507 <drew.jones1998@gmail.com>
2023-04-20 19:53:47 -0700

    Add JS function for submitting XML request based on id of HTML input

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-export function attachListener() {}
+function submitInputData(id) {
+    const data = document.getElementById(id).value;
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.open("POST", "index.php");
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            console.log(xhr.responseText);
+        }
+    };
+
+    xhr.send("data=" + data);
+}
